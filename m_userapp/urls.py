102a99ca51cae0c5c6b561eb41542034a9ed3137
M.Hatami <m.hatami@son.ir>
2021-08-10 16:17:43 +0430

    create simple calc

--- m_userapp/urls.py
+++ m_userapp/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('users/', views.users),
+    path('calc/', views.calc),
 ]
--- m_userapp/views.py
+++ m_userapp/views.py
@@ -1,7 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view()
 def users(request):
     return Response({"test": "test1"})
+
+
+@api_view(['GET', 'POST'])
+def calc(request):
+    try:
+        if request.method == "POST":
+            num1 = request.data['num1']
+            num2 = request.data['num2']
+            if num1 - num2 < 0:
+                return Response({"result": "Navigate"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"result": "Positive"}, status=status.HTTP_200_OK)
+        return Response({"result": "GET"}, status=status.HTTP_200_OK)
+    except KeyError:
+        return Response({"result": "Error"}, status=status.HTTP_400_BAD_REQUEST)
